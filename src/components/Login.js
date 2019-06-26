652746050d54b31b8b30198e47efcd32ecc2c378
byteconde <iamdeyvi@gmail.com>
2019-06-26 18:17:44 -0500

    Create Login Component

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+/** @jsx jsx */
+import React from "react";
+import { jsx } from "@emotion/core";
+
+function Login() {
+  return (
+    <div>
+      <p>Hola</p>
+    </div>
+  );
+}
+
+export default Login;
