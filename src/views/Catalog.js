3dc9ed7e47da79361a3ef634f80e2e89447332bf
Lian Nivin <liamrn94@gmail.com>
2019-06-27 18:59:27 -0500

    Add component with a bug

--- src/views/Catalog.js
+++ src/views/Catalog.js
@@ -1,16 +1,10 @@
 /** @jsx jsx */
 import React from "react";
 import { jsx } from "@emotion/core";
-<<<<<<< HEAD
 import { Redirect } from "@reach/router";
 import RestaurantHeader from "../components/RestaurantHeader";
 import RestaurantList from "../components/RestaurantList";
 import { useUser } from "../selector";
-=======
-
-import RestaurantHeader from "../components/RestaurantHeader";
-import RestaurantList from "../components/RestaurantList";
->>>>>>> Add header & Basic component
 
 function Catalog() {
   const user = useUser();
