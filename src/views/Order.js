318911f7ec3ca0765e69e662e06915f83ace2999
byteconde <iamdeyvi@gmail.com>
2019-06-27 19:02:17 -0500

    Render Ordered items

--- src/views/Order.js
+++ src/views/Order.js
@@ -1,13 +1,30 @@
+/** @jsx jsx */
 import React from "react";
 import { jsx } from "@emotion/core";
 import Product from "../components/product";
 import { useSelectorCart } from "../selector";
-import { connect } from "react-redux";
+import OrderItem from "./OrderItem";
 
-function Order() {
+function Order({
+  description = "descripcion",
+  name = "name",
+  price = "price"
+}) {
   const cartProducts = useSelectorCart();
   console.log(cartProducts);
-  return <div />;
+  return (
+    <div>
+      {Object.values(cartProducts).map(value => {
+        return (
+          <OrderItem
+            name={value.name}
+            price={value.price}
+            description={value.description}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default Order;
---
+++ src/views/OrderItem.js
@@ -0,0 +1,34 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
+import React from "react";
+
+function OrderItem({ name, price, description }) {
+  return (
+    <div
+      css={{
+        borderRadius: "15px",
+        backgroundColor: "#eee",
+        padding: "10px",
+        margin: "15px 0",
+        fontFamily: "Arial",
+        boxShadow: "3px 3px 7px 0px rgba(0,0,0,1)"
+      }}
+    >
+      <div />
+      <div
+        css={{
+          display: "flex",
+          justifyContent: "space-between",
+          paddingTop: "10px"
+        }}
+      >
+        <span css={{ fontSize: "1.2em", fontWeight: "bold" }}>{name}</span>
+        <span css={{ fontSize: "1.1em" }}>S/.{price}</span>
+      </div>
+
+      <p>{description}</p>
+    </div>
+  );
+}
+
+export default OrderItem;
