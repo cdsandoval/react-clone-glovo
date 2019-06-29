a81fb54d82579645d8d08445c35d42c606f98048
byteconde <iamdeyvi@gmail.com>
2019-06-28 22:14:34 -0500

    Add correct paths to feature header bar

--- src/views/productList.jsx
+++ src/views/productList.jsx
@@ -2,8 +2,46 @@
 import React from "react";
 import Product from "../components/product";
 import { jsx } from "@emotion/core";
+<<<<<<< HEAD
 import { Link, Redirect } from "@reach/router";
 import { useMenu, useUser } from "../selector";
+=======
+import { Link } from "@reach/router";
+import HeaderBar from "../components/HeaderBar";
+
+const menu_items = {
+  1: {
+    id: 1,
+    name: "Lomo Saltado",
+    description: "Rico lomo saltado",
+    price: 10
+  },
+  2: {
+    id: 2,
+    name: "Aji de Gallina",
+    description: "Rico Aji de Gallina",
+    price: 20
+  },
+  3: {
+    id: 3,
+    name: "Arroz con pollo",
+    description: "Rico arroz con pollo",
+    price: 15
+  },
+  4: {
+    id: 4,
+    name: "Chaufa",
+    description: "Rico Chaufa",
+    price: 25
+  },
+  5: {
+    id: 5,
+    name: "Caldo de Gallina",
+    description: "Rico caldo de gallina",
+    price: 30
+  }
+};
+>>>>>>> Add correct paths to feature header bar
 
 function ProductList() {
   const use = useMenu();
