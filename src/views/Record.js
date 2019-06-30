d7086a585c56d07a350c0a943c2531515da5358b
Carlos Sandoval <iznomag@gmail.com>
2019-06-29 23:51:36 -0500

    Add fetch order

--- src/views/Record.js
+++ src/views/Record.js
@@ -4,8 +4,14 @@ import { jsx } from "@emotion/core";
 
 import HistoryHeader from "../components/HistoryHeader";
 import HistoryList from "../components/HistoryList";
+import { useFetchOrder } from "../action-hook";
 
 function Record() {
+  const fetchOrder = useFetchOrder();
+
+  React.useEffect(() => {
+    fetchOrder();
+  });
   return (
     <div>
       <HistoryHeader />
